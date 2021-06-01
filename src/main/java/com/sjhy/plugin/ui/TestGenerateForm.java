f0335a1d1e2965eae7480dd73af7f2ced41ee88a
lihu <1449488533qq@gmail.com>
2021-06-01 17:38:13 +0800

    Add test generate form

---
+++ src/main/java/com/sjhy/plugin/ui/TestGenerateForm.java
@@ -0,0 +1,15 @@
+package com.sjhy.plugin.ui;
+
+import javax.swing.*;
+
+/**
+ * @author lihu@eventslack.com
+ * @since 2021/5/24
+ */
+public class TestGenerateForm {
+    private JPanel contentPannel;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JComboBox comboBox2;
+    private JComboBox comboBox3;
+}
