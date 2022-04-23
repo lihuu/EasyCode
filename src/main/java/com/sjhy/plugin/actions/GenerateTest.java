d1cd90fc4b0039a6c02512e3df8c770511f7e547
SilentStorm <1449488533qq@gmail.com>
2022-04-23 22:18:10 +0800

    重构生成测试文件代码

--- src/main/java/com/sjhy/plugin/actions/GenerateTest.java
+++ src/main/java/com/sjhy/plugin/actions/GenerateTest.java
@@ -4,7 +4,6 @@ import com.intellij.lang.jvm.annotation.JvmAnnotationConstantValue;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.PsiMethodImpl;
@@ -12,11 +11,10 @@ import com.sjhy.plugin.comm.TargetTestFileNotFoundException;
 import com.sjhy.plugin.config.Settings;
 import com.sjhy.plugin.entity.*;
 import com.sjhy.plugin.service.CodeGenerateService;
-import org.apache.commons.codec.language.bm.Lang;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Method;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -32,75 +30,91 @@ public class GenerateTest extends AnAction {
         }
         PsiElement psiElement = e.getData(LangDataKeys.PSI_ELEMENT);
         if (psiElement instanceof PsiMethodImpl) {
-            //方法
-            PsiMethodImpl targetMethod = (PsiMethodImpl) psiElement;
-            String methodName = targetMethod.getName();
-            PsiAnnotation[] annotations = targetMethod.getAnnotations();
-            //解析方法上面的注解
-            List<AnnotationInfo> methodAnnotationInfoList = buildAnnotationInfoList(annotations);
-            PsiParameterList parameterList = targetMethod.getParameterList();
-            PsiClass containingClass = targetMethod.getContainingClass();
-            String containingClassName;
-            String qualifiedName;
-            if (containingClass != null) {
-                containingClassName = containingClass.getName();
-                qualifiedName = Optional.ofNullable(containingClass.getQualifiedName()).orElse("");
-            } else {
-                containingClassName = "";
-                qualifiedName = "";
-            }
-            ClassInfo classInfo = new ClassInfo(containingClassName, qualifiedName.substring(0, qualifiedName.lastIndexOf(".")));
-            classInfo.setOpenFile(false);
-            classInfo.setAnnotationInfoList(buildAnnotationInfoList(containingClass.getAnnotations()));
-            MethodInfo methodInfo = MethodInfo.builder()
-                    .methodName(methodName)
-                    .containingClassName(containingClassName)
-                    .classInfo(classInfo)
-                    .annotationInfos(methodAnnotationInfoList)
-                    .methodParameters(Stream.of(parameterList.getParameters()).map(
-                            psiParameter -> 
-                                PropertyInfo.builder().name(psiParameter.getName()).type(psiParameter.getType().getCanonicalText())
-                                    .shortType(psiParameter.getType().getPresentableText()).build()).collect(Collectors.toList())
-                    ).build();
-            Template template =
-                    Settings.getInstance().getTemplateGroupMap().get("Test").getElementList().stream().filter(t -> "test.method".equals(t.getName())).findFirst()
-                            .orElseThrow(() -> new RuntimeException("test.method 模板文件不存在"));
-            try {
-                CodeGenerateService.getInstance(project).generateTestCode(template, methodInfo);
-            } catch (TargetTestFileNotFoundException targetTestFileNotFoundException) {
-                //可能对应的文件不存在，如果不存在就先创建
-                Template testClassTemplate =
-                        Settings.getInstance().getTemplateGroupMap().get("Test").getElementList().stream().filter(t -> "test.common".equals(t.getName())).findFirst()
-                                .orElseThrow(() -> new RuntimeException("test.common 模板文件不存在"));
-                CodeGenerateService.getInstance(project).generateTestCode(testClassTemplate, methodInfo.getClassInfo());
-                CodeGenerateService.getInstance(project).generateTestCode(template, methodInfo);
-            }
-
+            generateTestMethod(project, (PsiMethodImpl)psiElement);
         } else if (psiElement instanceof PsiClass) {
             generateTestClass(project, (PsiClass)psiElement);
         } else {
             PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
             if (psiFile instanceof PsiJavaFile) {
-                PsiJavaFile psiJavaFile = (PsiJavaFile) psiFile;
-                String classFileName = psiJavaFile.getName();
-                ClassInfo classInfo = new ClassInfo(classFileName.substring(0, classFileName.indexOf(".")), psiJavaFile.getPackageName());
-                Template template = Settings.getInstance().getTemplateGroupMap().get("Test").getElementList().stream().filter(t -> "test.common".equals(t.getName())).findFirst()
-                        .orElseThrow(() -> new RuntimeException("模板不存在"));
-                CodeGenerateService.getInstance(project).generateTestCode(template, classInfo);
+                generateTestFile(project, (PsiJavaFile)psiFile);
             }
         }
     }
 
-    private void generateTestClass(Project project, PsiClass psiElement) {
-        String name = psiElement.getName();
-        String qualifiedName = psiElement.getQualifiedName();
+    private void generateTestFile(Project project, PsiJavaFile psiJavaFile) {
+        String classFileName = psiJavaFile.getName();
+        ClassInfo classInfo = new ClassInfo(classFileName.substring(0, classFileName.indexOf(".")), psiJavaFile.getPackageName());
+        Template template = getTemplate("test.common");
+        CodeGenerateService.getInstance(project).generateTestCode(template, classInfo);
+    }
+
+    private void generateTestMethod(Project project, PsiMethodImpl psiMethod) {
+        String methodName = psiMethod.getName();
+        PsiAnnotation[] annotations = psiMethod.getAnnotations();
+        //解析方法上面的注解
+        List<AnnotationInfo> methodAnnotationInfoList = buildAnnotationInfoList(annotations);
+        PsiParameterList parameterList = psiMethod.getParameterList();
+        PsiClass containingClass = psiMethod.getContainingClass();
+        String containingClassName;
+        String qualifiedName;
+        if (containingClass != null) {
+            containingClassName = containingClass.getName();
+            qualifiedName = Optional.ofNullable(containingClass.getQualifiedName()).orElse("");
+        } else {
+            containingClassName = "";
+            qualifiedName = "";
+        }
+        ClassInfo classInfo = new ClassInfo(containingClassName, qualifiedName.substring(0, qualifiedName.lastIndexOf(".")));
+        classInfo.setOpenFile(false);
+        classInfo.setAnnotationInfoList(buildAnnotationInfoList(containingClass.getAnnotations()));
+        MethodInfo methodInfo = MethodInfo.builder()
+            .methodName(methodName)
+            .containingClassName(containingClassName)
+            .classInfo(classInfo)
+            .annotationInfos(methodAnnotationInfoList)
+            .methodParameters(toMethodParameters(parameterList)
+            ).build();
+        Template template = getTemplate("test.method");
+        try {
+            CodeGenerateService.getInstance(project).generateTestCode(template, methodInfo);
+        } catch (TargetTestFileNotFoundException targetTestFileNotFoundException) {
+            //可能对应的文件不存在，如果不存在就先创建
+            Template testClassTemplate = getTemplate("test.common");
+            CodeGenerateService.getInstance(project).generateTestCode(testClassTemplate, methodInfo.getClassInfo());
+            CodeGenerateService.getInstance(project).generateTestCode(template, methodInfo);
+        }
+    }
+
+    private Template getTemplate(String templateName) {
+        return Settings.getInstance().getTemplateGroupMap().get("Test")
+            .getElementList().stream()
+            .filter(t -> templateName.equals(t.getName()))
+            .findFirst().orElseThrow(() -> new RuntimeException("模板内容不存在"));
+    }
+
+    @NotNull
+    private List<PropertyInfo> toMethodParameters(PsiParameterList parameterList) {
+        return Stream.of(parameterList.getParameters()).map(toProperInfo()).collect(Collectors.toList());
+    }
+
+    @NotNull
+    private Function<PsiParameter, PropertyInfo> toProperInfo() {
+        return psiParameter -> PropertyInfo.builder()
+            .name(psiParameter.getName())
+            .type(psiParameter.getType().getCanonicalText()) 
+            .shortType(psiParameter.getType().getPresentableText())
+            .build();
+    }
+
+    private void generateTestClass(Project project, PsiClass psiClass) {
+        String name = psiClass.getName();
+        String qualifiedName = psiClass.getQualifiedName();
         if (qualifiedName == null) {
             return;
         }
         //文件创建所有的
         ClassInfo classInfo = new ClassInfo(name, qualifiedName.substring(0, qualifiedName.lastIndexOf(".")));
-        Template template = Settings.getInstance().getTemplateGroupMap().get("Test").getElementList().stream().filter(t -> "test.common".equals(t.getName())).findFirst()
-                .orElseThrow(() -> new RuntimeException("模板不存在"));
+        Template template = getTemplate("test.common");
         CodeGenerateService.getInstance(project).generateTestCode(template, classInfo);
     }
 
@@ -109,8 +123,10 @@ public class GenerateTest extends AnAction {
             return Arrays.stream(psiAnnotations).map(psiAnnotation -> {
                 String annotationQualifiedName = psiAnnotation.getQualifiedName();
                 Map<String, Object> annotationValues = Arrays.stream(psiAnnotation.getParameterList().getAttributes())
-                        .filter(psiNameValuePair -> psiNameValuePair.getAttributeValue() != null && psiNameValuePair.getAttributeValue() instanceof JvmAnnotationConstantValue)
-                        .collect(Collectors.toMap(PsiNameValuePair::getAttributeName, psiNameValuePair -> Optional.ofNullable(((JvmAnnotationConstantValue) psiNameValuePair.getAttributeValue()).getConstantValue()).orElse(new Object()), (v1, v2) -> v2));
+                    .filter(psiNameValuePair -> psiNameValuePair.getAttributeValue() != null && psiNameValuePair.getAttributeValue() instanceof JvmAnnotationConstantValue)
+                    .collect(Collectors.toMap(PsiNameValuePair::getAttributeName,
+                        psiNameValuePair -> Optional.ofNullable(((JvmAnnotationConstantValue)psiNameValuePair.getAttributeValue()).getConstantValue()).orElse(new Object()),
+                        (v1, v2) -> v2));
                 return AnnotationInfo.builder().name(annotationQualifiedName).annotationValues(annotationValues).build();
             }).collect(Collectors.toList());
         } catch (Exception e) {
