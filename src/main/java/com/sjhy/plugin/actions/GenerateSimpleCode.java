f72c7bf0a9fd8b491f804a2855f8d5810bae5c1c
lihu <1449488533qq@gmail.com>
2022-09-15 14:42:25 +0800

    fix: fill in all properties

--- src/main/java/com/sjhy/plugin/actions/GenerateSimpleCode.java
+++ src/main/java/com/sjhy/plugin/actions/GenerateSimpleCode.java
@@ -16,6 +16,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 可以根据entity自动生成对应的代码，不用从数据库中获取
@@ -37,21 +38,24 @@ public class GenerateSimpleCode extends AnAction {
         }
         String classFileName = psiJavaFile.getName();
         ClassInfo classInfo = new ClassInfo(classFileName.substring(0, classFileName.indexOf(".")), psiJavaFile.getPackageName());
-        List<PsiField> psiFieldList = Arrays.stream(psiJavaFile.getClasses()[0].getAllFields()).filter(f -> f.hasAnnotation("javax.persistence.Id")).collect(Collectors.toList());
+        Stream<PsiField> psiFieldStream = Arrays.stream(psiJavaFile.getClasses()[0].getAllFields());
+        List<PsiField> allFields = psiFieldStream.collect(Collectors.toList());
+        List<PsiField> psiFieldList = allFields.stream().filter(f -> f.hasAnnotation("javax.persistence.Id")).collect(Collectors.toList());
         if (!psiFieldList.isEmpty()) {
-            List<PropertyInfo> propertyInfos = psiFieldList.stream().map(psiField -> {
-                String name = psiField.getName();
-                String type = psiField.getType().getCanonicalText();
-                return PropertyInfo.builder()
-                    .type(type)
-                    .shortType(type.substring(type.lastIndexOf(".") + 1))
-                    .name(name)
-                    .build();
-            }).collect(Collectors.toList());
-            classInfo.setAllProperties(propertyInfos);
-            classInfo.setPrimaryKeyProperties(propertyInfos);
+            classInfo.setPrimaryKeyProperties(psiFieldList.stream().map(GenerateSimpleCode::toPropertyInfo).collect(Collectors.toList()));
         }
+        classInfo.setAllProperties(allFields.stream().map(GenerateSimpleCode::toPropertyInfo).collect(Collectors.toList()));
         new CodeGenerateForm(project, classInfo).open();
+    }
+
+    private static PropertyInfo toPropertyInfo(PsiField psiField) {
+        String name = psiField.getName();
+        String type = psiField.getType().getCanonicalText();
+        return PropertyInfo.builder()
+            .type(type)
+            .shortType(type.substring(type.lastIndexOf(".") + 1))
+            .name(name)
+            .build();
     }
 
     @Nullable
